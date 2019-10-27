45882f587bb9eba2d91fcf846c9cc0300ddbe580
Krzysztof Kaczmarski <45950522+Kaczmar861@users.noreply.github.com>
2019-10-27 20:01:35 +0100

    adding static fields and variables

--- app/LibraryApp.java
+++ app/LibraryApp.java
@@ -1,9 +1,11 @@
 package app;
 
 public class LibraryApp{
+    private static final String APP_NAME = "Biblioteka v 1.0";
+    
     public static void main(String[] args){
-        final String appName = "Biblioteka v 0.9";
-        System.out.println(appName);
+        
+        System.out.println(APP_NAME);
         LibraryControl libControl = new LibraryControl();
         libControl.controlLoop();
     }
